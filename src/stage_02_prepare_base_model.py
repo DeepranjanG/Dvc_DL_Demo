e6f2931818d887aa8a759e58b54881e9c5bf25e5
DeepranjanG <kdeepu070@gmail.com>
2021-10-25 11:10:55 +0530

    stage 03 base file ready

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,10 +1,9 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import get_VGG_16_model
+from src.utils.models import get_VGG_16_model, prepare_model
 import argparse
-import pandas as pd
+import io
 import os
-import shutil
-from tqdm import tqdm
+
 import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
@@ -25,12 +24,35 @@ def prepare_base_model(config_path, params_path):
 
     base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
 
-    create_directory([base_model_dir])
+    create_directory([base_model_dir_path])
 
     base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
     model = get_VGG_16_model(input_shape=params['IMAGE_SIZE'], model_path=base_model_path)
 
+    full_model = prepare_model(
+        model,
+        CLASSES=params['CLASSES'],
+        freeze_all=True,
+        freeze_till=None,
+        learning_rate=params['LEARNING_RATE']
+    )
+
+    update_base_model_path = os.path.join(
+        base_model_dir_path,
+        artifacts["UPDATED_BASE_MODEL_NAME"]
+    )
+
+    def _log_model_summary(model):
+        with io.StringIO() as stream:
+            model.summary(print_fn=lambda x: stream.write(f"{x}\n"))
+            summary_str = stream.getvalue()
+        return summary_str
+
+
+    logging.info(f"full model summary: \n{_log_model_summary(full_model)}")
+
+    full_model.save(update_base_model_path)
 
 
 
@@ -47,7 +69,7 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
     try:
-        logging.info(">>>>>> stage one started")
+        logging.info(">>>>>> stage two started")
         prepare_base_model(config_path=parsed_args.config, params_path=parsed_args.params)
         logging.info("stage two completed! Base model is created >>>>>>>")
     except Exception as e:
